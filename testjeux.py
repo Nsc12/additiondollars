104fbb31f033f5fdcfee7d82ff3325b942da3489
Nesc <103075347+Nsc12@users.noreply.github.com>
2022-07-30 00:31:03 +0200

    Add files via upload

---
+++ testjeux.py
@@ -0,0 +1,78 @@
+
+
+print("Jeuxdollarsaddition")
+print("")
+print("")
+
+
+n = int(input("$ $ $ + $ $ = "))
+while n != 5:
+    print("X")
+    n = int(input("$ $ $ + $ $ = "))
+if n == 5:
+    print("V")
+
+
+n = int(input("$ $ $ $ + $ $ = "))
+while n != 6:
+    print("X")
+    n = int(input("$ $ $ $ + $ $ = "))
+if n == 6:
+    print("V")
+
+n = int(input("$ $ $ $ $ + $ $ = "))
+while n != 7:
+    print("X")
+    n = int(input("$ $ $ $ $ + $ $ = "))
+if n == 7:
+    print("V")
+
+n = int(input("$ $ $ $ $ $ + $ $ = "))
+while n != 8:
+    print("X")
+    n = int(input("$ $ $ $ $ $ + $ $ = "))
+if n == 8:
+    print("V")
+
+n = int(input("$ $ $ $ $ $ + $ $ $ = "))
+while n != 9:
+    print("X")
+    n = int(input("$ $ $ $ $ $ + $ $ $ = "))
+if n == 9:
+    print("V")
+
+n = int(input("$ $ $ $ $ $ + $ $ $ $ = "))
+while n != 10:
+    print("X")
+    n = int(input("$ $ $ $ $ $ + $ $ $ $ = "))
+if n == 10:
+    print("V")
+
+n = int(input("$ $ $ $ + $ $ $ $ = "))
+while n != 8:
+    print("X")
+    n = int(input("$ $ $ $  + $ $ $ $ = "))
+if n == 8:
+    print("V")
+
+n = int(input("$ $ $ $ $ + $ $ $ $ = "))
+while n != 9:
+    print("X")
+    n = int(input("$ $ $ $ $ + $ $ $ $ = "))
+if n == 9:
+    print("V")
+n = int(input("$ $ $ $ + $ $ $ = "))
+while n != 7:
+    print("X")
+    n = int(input("$ $ $ $ + $ $ $ = "))
+if n == 7:
+    print("V")
+
+
+n = int(input("$ $ $ $ $ + $ $ $ $ $ = "))
+while n != 10:
+    print("X")
+    n = int(input("$ $ $ $ $ + $ $ $ $ $ = "))
+if n == 10:
+    
+    print("V")
